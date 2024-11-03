256ef7086b17ef20aa282c7ad3a63130bf589e8a
JunhaoQiu <56094690+qchiujunhao@users.noreply.github.com>
2024-11-03 18:03:25 -0500

    see description
    1. enable users to user their own Groq api key
    2. fixed table displaying issue in html
    3. adding more example questions
    4. adding a new history entry of plots
    5. prepare for deploying on main

--- config.py
+++ config.py
@@ -8,6 +8,22 @@ supported_chat_models = [
     "gpt-3.5-turbo",
 ]
 
+groq_models = [
+    "llama-3.2-90b-text-preview",
+    "llama-3.2-90b-vision-preview",
+    "llama-3.1-70b-versatile",
+]
+
+def get_groq_key(groq_api_key_user):
+    if not groq_api_key_user:
+        groq_api_key_user = st.sidebar.text_input(label="Your Groq API Key:", type="password")
+    if not groq_api_key_user:
+        st.error("Groq API key not set for your chat")
+        st.stop()
+    
+    model = st.sidebar.selectbox(label="Select the model you want", options=groq_models)
+    return model, groq_api_key_user
+
 def get_openai_key(openai_api_key):
     if not openai_api_key:
         openai_api_key = st.sidebar.text_input(label="Your OpenAI API Key:", type="password")
@@ -25,11 +41,16 @@ def get_openai_key(openai_api_key):
     model = st.sidebar.selectbox(label="Select the model you want", options=supported_chat_models)
     return model, openai_api_key
 
-def configure_llm_options(openai_api_key):
-    available_options = ["Groq", "GPT-4o-mini", "GPT-4o", "BambooLLM", "OpenAI", "Your BambooLLM API Key"]
-    default_index = 0 if not openai_api_key else 2
+def configure_llm_options(openai_api_key, groq_api_key_user):
+    available_options = ["llama-3.2-90b", "GPT-4o-mini", "GPT-4o", "BambooLLM", "OpenAI", "Your-BambooLLM-API-Key", "Your-Groq-API-Key"]
+    default_index = 0 
+    if groq_api_key_user:
+        default_index = 6
+    elif openai_api_key:
+        default_index = 4
+
     llm_choice = st.sidebar.radio(
-        label="Select LLM for analysis:", 
+        label="Select a LLM for analysis (You are encouraged to use your own API keys. See below for more information):", 
         options=available_options, 
         index=default_index)
     
@@ -37,25 +58,29 @@ def configure_llm_options(openai_api_key):
         return llm_choice, None, None
     elif llm_choice == "GPT-4o-mini":
         return llm_choice, None, None
-    elif llm_choice == "Groq":
-        return llm_choice, None, None
+    elif llm_choice == "llama-3.2-90b":
+        return "Groq", None, None
     elif llm_choice == "OpenAI":
         model, openai_api_key = get_openai_key(openai_api_key)
         return llm_choice, model, openai_api_key
     elif llm_choice == "BambooLLM":
         return llm_choice, None, None
-    else:
+    elif llm_choice == "Your-BambooLLM-API-Key":
         bamboollm_key = st.sidebar.text_input(label="Your BambooLLM API Key:", type="password")
         if not bamboollm_key:
             st.error("BambooLLM API key not set for your chat")
             st.stop()
         return "BambooLLM", None, bamboollm_key
+    elif llm_choice == "Your-Groq-API-Key":
+        model, groq_api_key_user = get_groq_key(groq_api_key_user)
+        return llm_choice, model, groq_api_key_user
 
 def display_example_questions():
     with st.sidebar:
-        
         st.divider()
-        st.write("You can get your free API key for BambooLLM or Groq signing up at https://pandas-ai.com or https://groq.com")
+        st.write("You can get your free API key for BambooLLM by signing up at https://pandas-ai.com and your free API key for Groq by signing up at https://groq.com.")
+        
+        
         st.divider()
         st.markdown("## Click on an Example Question to Try the App")
     #     example_questions = [
@@ -101,7 +126,9 @@ def display_example_questions():
 
         example_questions = [
             "Tell me something interesting about the dataset in a plot?",
-            "Summarize the dataset",
+            "Delete the columns where most of the values are missing and return the modified dataset",
+            "Return the types of columns in a dataframe.",
+            "Provide the statistics for each column in a properly formatted DataFrame. The DataFrame must include a column named 'column_name' that contains the name of each column.",
             "Are there any missing values in the dataset? If so, which columns have them?",
             "Create a histogram for any column?",
             "Provide a scatterplot for any two columns?"
@@ -118,12 +145,9 @@ def display_example_questions():
 def display_notes():
     with st.sidebar:
         st.divider()
-        st.write("You can get your free API key for BambooLLM igning up at https://pandas-ai.com ")
-        st.divider()
         st.markdown("## Important Notes")
         st.markdown("#### The tool can make mistakes!")
-        st.markdown("#### The Groq model is llama-3.2-90b-text-preview")
-        st.markdown("#### If the answers are not good from Groq and BambooLLM, you could consider OpenAI.")
+        st.markdown("#### If the answers are not satisfactory from one LLM, you could consider switching to another LLM or asking a more specific question.")
 
 
         
